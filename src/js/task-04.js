b5ed562dac317e5fce712e6069bd8041fb2f1b97
almacoryi <almacoriy@gmail.com>
2021-12-05 16:19:37 +0200

    fix task 04

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,4 +1,5 @@
 let counterValue = 0;
+let counter = 0;
 
 const refs = {
   decrement: document.querySelector("#counter button[data-action='decrement']"),
@@ -8,7 +9,9 @@ const refs = {
 
 refs.decrement.addEventListener("click", () => changeValue(-1));
 refs.increment.addEventListener("click", () => changeValue(+1));
+refs.value.textContent = counter;
 
 function changeValue(counterValue) {
-  value.textContent = parseInt(value.textContent, 10) + counterValue;
+  counter += counterValue;
+  refs.value.textContent = counter;
 }
